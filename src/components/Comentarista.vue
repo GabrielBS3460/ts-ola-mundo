8cc6f2a25beda5c46cc6923c9915055bf5bb6327
Gabriel Borges <gabriel.borges@owse.com.br>
2023-09-21 15:46:59 -0300

    Adicionando Arquivos

---
+++ src/components/Comentarista.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <button @click="comentar">
+            Comentar
+        </button>
+        <p>
+            {{ comentario }}
+        </p>
+        <p>
+            {{ detalhes }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+@Component
+export default class Comentarista extends Vue{
+    public comentario = ''
+    @Prop() public nome!: string
+
+    public comentar(): void{
+        this.comentario = 'Um mega comentário bem bacana!'
+    }
+
+    get detalhes(){
+        return `Nome: ${this.nome}`
+    }
+}
+
+</script>
